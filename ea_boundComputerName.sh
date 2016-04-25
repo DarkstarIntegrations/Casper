a2a8b845e3f46b356b1f4527fe8ec6877c386b64
Darkstar Integrations <DarkstarIntegrations@users.noreply.github.com>
2016-04-24 23:56:41 -0500

    Create ea_boundComputerName.sh
    This script is for use with JAMF Casper as an Extension Attribute. This script is meant to be used on bound machines only

---
+++ ea_boundComputerName.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Name: ea_boundComputerName.sh
+# Author: matt.lee@darkstarintegrations.com
+# Description: Displays bound Active Directory name as an Extension Attribute
+
+# Get Active Directory bound name
+boundName=`dsconfigad -show | awk '/Computer Account/{print $NF}' | tr '[a-z]' '[A-Z]' | sed s/.$//`
+
+# Script Contents Below
+if [ "$boundName" == "" ] ; then
+  echo "<result>Not Bound</result>"
+    else
+  echo "<result>$boundName</result>"
+fi
+
+exit 0
